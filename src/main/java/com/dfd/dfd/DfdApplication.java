121df6d2676b99d12ca584359d932cf1d75108d2
turhanomer <omerturhan16@outlook.com>
2025-06-03 00:25:40 +0300

    Initial commit

---
+++ src/main/java/com/dfd/dfd/DfdApplication.java
@@ -0,0 +1,13 @@
+package com.dfd.dfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dfd/dfd/DfdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfd.dfd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
